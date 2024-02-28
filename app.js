dd54b78728e41647e97fa7cb6e5f6d46326034c0
hasnainabbasi20 <154608171+hasnainabbasi20@users.noreply.github.com>
2024-02-28 17:18:21 +0500

    js assignment

---
+++ app.js
@@ -0,0 +1,295 @@
+// Chapter # 1
+// Alert
+
+// Question # 1
+
+// alert('Welcome to our Website')
+
+// Question # 2
+
+// alert('your email or username is invalid ')
+
+// Question # 3
+
+// alert('Welcome to \n Hasnain Habib \n JS coding')
+
+// // Question # 4
+
+// alert('Welcome to JS land \n')
+
+// alert('Happy coding')
+
+
+// // Chapter # 2
+
+// // Question # 6
+
+// var numberOfChildren= 3
+// var partnerName= 'Ansa'
+// var geographic= 'Karachi'
+// var jobTitle= 'SoftwareDeveloper'
+
+// alert(`I will be a ${jobTitle} in ${geographic} and married to ${partnerName} with ${numberOfChildren} kids.`)
+
+
+
+// Question # 7
+
+
+// var email= "hasnainhabib842@gmail.com"
+// alert(`My email address is ${email}`)
+
+
+// // Question # 8
+
+// var book= "A smart way to learn JavaScript"
+// alert(`I am trying to learrn from the book'${book}'`)
+
+
+// Chapter # 3
+
+// Question # 1
+
+
+// var myAge= 20
+
+// alert(`I am ${myAge} year old`)
+
+
+// // Question # 3
+
+// var birthYear= 2003
+
+// document.write(`My birth year is ${birthYear} <br> Data type of my declared variable is ${typeof(birthYear)} `)
+
+// Question # 4
+
+var visitorName = 'M.Hasnain'
+var ProductTitle = 'watch'
+var productQuantity = +prompt('enter ordered quantity')
+
+document.write(`${visitorName} ordered ${productQuantity} ${ProductTitle} on ABC store`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Chapter # 5-7
+
+
+// // Question1 & 2
+
+// var num1 = +prompt('enter a number 1')
+// var num2 = +prompt('enter a value 2')
+
+// var sum = num1 + num2
+// var sub = num1 - num2
+// var multiply = num1 * num2
+// var divide = num1 / num2
+// var mode = num1 % num2
+
+// document.write
+//               (`<h1> the sum of number1 and number2 is ${sum}<br>the subtraction of number1 and number2 is ${sub}<br> the multiplication of number1 and number2 is ${multiply}<br> the division of number1 and number2 is ${divide} <br>the modulus of number1 and number2 is ${mode}<br> </h1>`)
+
+
+
+// // Question # 4
+// var oneTicketCost = 600
+// var totalTicketsBuy = 5
+
+// var costfTotalTickets = oneTicketCost * totalTicketsBuy
+
+// document.write(
+//     `Total cost to buy 5 tickets to a movie is ${costfTotalTickets}PKR`
+// )
+
+// // Question # 7
+
+// document.write('<h1>Shopping Cart</h1>')
+
+// var priceOfItem_1 = +prompt('enter rate of item_1')
+// var priceOfItem_2 = +prompt('enter rate of item_2')
+// var orderQuantityOfItem_1 = +prompt('enter order quantity of item_1')
+// var orderQuantityOfItem_2 = +prompt('enter order quantity of item_2')
+// var shippingCharges = +prompt('enter shipping charges')
+
+// var totalOf1stItem = priceOfItem_1 * orderQuantityOfItem_1
+
+// var totalOf2ndItem = priceOfItem_2 * orderQuantityOfItem_2
+
+// var total = totalOf1stItem + totalOf2ndItem 
+
+// var totalCost = total+shippingCharges
+
+// document.write(`Price of item 1 is ${priceOfItem_1} <br> Quantity of item 1 is ${orderQuantityOfItem_1} <br> price of item 2 is ${priceOfItem_2} <br> Quantity of itemn 2 is ${orderQuantityOfItem_2} <br> <br> Shipping charges ${shippingCharges} <br> Total cost of your order is ${totalCost}`)
+
+
+// // Question # 8
+
+// studenName=prompt('enter student name')
+// studentRollNumber=prompt('enter roll number')
+// var markObtained=prompt('enter obtained marks')
+// var totalMarks=prompt('enter a total marks')
+
+// var calculatePercentage=markObtained*totalMarks/100
+
+// document.write(`Student Name is :<br> ${studenName} <br> Student Roll number is: ${studentRollNumber} <br> Total Marks: ${totalMarks} <br> Obtained Marks: ${markObtained} <br> Percentage: ${calculatePercentage}`)
+
+
+// Question # 9
+
+// var usDollar =10
+// var saudiRiyals =25
+
+// var uSD_1 =285
+// var Riyal_1 =74.48
+
+// var convert =usDollar*uSD_1 + saudiRiyals * Riyal_1
+
+// document.write (`Total currency in PKR ${convert}`)
+
+
+// Question # 10
+
+// var num=5
+// var result = num + 5 * 10 /2
+
+// alert(`show result ${result}`)
+
+
+// Question # 11
+
+// document.write('<h1>The Age Calulator</h1> <br>')
+
+// var currentYear= 2023
+// var birthYear= 2003
+
+// var age1=currentYear-birthYear
+// var age2=age1 + 1
+
+// document.write(`Current Year : ${currentYear} <br> Birth Year : ${birthYear} <br> <br> They are either ${age1} or ${age2} years old`)
+
+
+// Question # 12
+
+
+// var radius=20
+
+// const pi=3.142
+// var circumference = 2*pi*radius
+// var area = pi*(radius*radius)
+
+// document.write(`<h1>Geometirize</h1> <br> <br>Radius of a circle is ${radius} <br> The circumference of circle is ${circumference} <br> The area of a circle is ${area}`)
